18f228fd7039d2fc5bc161a22917aacaf6e84b13
Rahul Agarwal <36710496+rahulag118@users.noreply.github.com>
2021-06-01 23:18:36 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws"{
+region="eu-west-2"
+}
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
